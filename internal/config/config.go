c353a750925c9fd06a194146d88c36d25757b17c
Osamikoyo <v.pylayev@mail.ru>
2025-06-06 06:45:23 +0500

    update config

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,32 +7,32 @@ type (
 
 	Queues map[string]string
 
-	DB struct{
-		Host string
-		Port string
-		User string
+	DB struct {
+		Host     string
+		Port     string
+		User     string
 		Password string
-		DBName string
+		DBName   string
 	}
 
-	HealthCheck struct{
+	HealthCheck struct {
 		Port string
-		Use bool
+		Use  bool
 	}
 
-	RetrierOpts struct{
+	RetrierOpts struct {
 		MaxRetries int
-		Interval time.Duration
+		Interval   time.Duration
 	}
 
 	Urls map[string]string
 
-	Config struct{
-		Exchanges Exchanges
-		Queues Queues
-		DB DB
+	Config struct {
+		Exchanges   Exchanges
+		Queues      Queues
+		DB          DB
 		RetrierOpts RetrierOpts
-		Urls Urls
+		Urls        Urls
 		HealthCheck HealthCheck
 	}
 )
@@ -40,31 +40,31 @@ type (
 func NewConfig() *Config {
 	return &Config{
 		Exchanges: Exchanges{
-			"form": "form",
+			"form":   "form",
 			"answer": "answer",
 		},
 		Queues: Queues{
-			"form": "form",
+			"form":   "form",
 			"answer": "answer",
 		},
 		DB: DB{
-			Host: "localhost",
-			Port: "5432",
-			User: "postgres",
+			Host:     "localhost",
+			Port:     "5432",
+			User:     "postgres",
 			Password: "postgres",
-			DBName: "postgres",
+			DBName:   "postgres",
 		},
 		RetrierOpts: RetrierOpts{
 			MaxRetries: 3,
-			Interval: 5 * time.Second,
+			Interval:   5 * time.Second,
 		},
 		Urls: Urls{
 			"rabbitmq": "amqp://rabbitmq:5672",
-			"redis": "redis:6379",
+			"redis":    "redis:6379",
 		},
 		HealthCheck: HealthCheck{
 			Port: "8080",
-			Use: true,
+			Use:  true,
 		},
 	}
 }
