32cd254734e54817dcb7daa80b978a06a0962962
Osamikoyo <v.pylayev@mail.ru>
2025-06-06 06:44:59 +0500

    update logger

--- cmd/answer-service/main.go
+++ cmd/answer-service/main.go
@@ -1,5 +1,73 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/Koyo-os/answer-service/internal/config"
+	"github.com/Koyo-os/answer-service/internal/entity"
+	"github.com/Koyo-os/answer-service/internal/repository"
+	"github.com/Koyo-os/answer-service/pkg/logger"
+	"github.com/Koyo-os/answer-service/pkg/retrier"
+	"go.uber.org/zap"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
 func main() {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, syscall.SIGINT, syscall.SIGTERM)
+
+	eventChan := make(chan entity.Event, 100) // Add buffer for better performance
+
+	logCfg := logger.Config{
+		LogFile:   "app.log",
+		LogLevel:  "debug",
+		AppName:   "answer-service",
+		AddCaller: true,
+	}
+
+	if err := logger.Init(logCfg); err != nil {
+		panic(err)
+	}
+
+	defer logger.Sync()
+
+	logger := logger.Get()
+
+	cfg := config.NewConfig()
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	logger.Info("connecting to mariadb...", zap.String("dsn", dsn))
+
+	db, err := retrier.Connect(10, 10, func() (*gorm.DB, error) {
+		return gorm.Open(mysql.Open(dsn))
+	})
+	if err != nil {
+		logger.Error("error initialyze database",
+			zap.String("dsn", dsn),
+			zap.Error(err))
+
+		return
+	}
+
+	logger.Info("connected to mariadb", zap.String("dsn", dsn))
+
+	if err := db.AutoMigrate(&entity.Answer{}, &entity.Element{}); err != nil {
+		logger.Error("failed to migrate database", zap.Error(err))
+		return
+	}
+
+	repo := repository.NewRepository(db, logger)
+
 	
 }
---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/Koyo-os/answer-service
+
+go 1.24.2
+
+require (
+	github.com/bytedance/sonic v1.13.3 // indirect
+	github.com/bytedance/sonic/loader v0.2.4 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/rabbitmq/amqp091-go v1.10.0 // indirect
+	github.com/redis/go-redis/v9 v9.9.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
+	go.uber.org/zap v1.27.0 // indirect
+	golang.org/x/arch v0.0.0-20210923205945-b76863e36670 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	gorm.io/gorm v1.30.0 // indirect
+)
