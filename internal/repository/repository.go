5231294e8621516a0e42c4884219e48d70fcec3a
Osamikoyo <v.pylayev@mail.ru>
2025-06-07 08:47:15 +0500

    update repository

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/Koyo-os/answer-service/internal/entity"
 	"github.com/Koyo-os/answer-service/pkg/logger"
@@ -49,36 +48,4 @@ func (repo *Repository) DeleteAnswer(ctx context.Context, id uuid.UUID) error {
 	}
 
 	return nil
-}
-
-func (repo *Repository) GetAnswerBy(ctx context.Context, key string, value interface{}) ([]entity.Answer, error) {
-	var answers []entity.Answer
-
-	res := repo.db.WithContext(ctx).Where(fmt.Sprintf("%s = ?", key), value).Find(&answers)
-
-	if err := res.Error; err != nil {
-		repo.logger.Error("error get answer",
-			zap.String("key", key),
-			zap.Error(err))
-
-		return nil, err
-	}
-
-	return answers, nil
-}
-
-func (repo *Repository) GetAnswer(ctx context.Context, id uuid.UUID) (*entity.Answer, error) {
-	answer := new(entity.Answer)
-
-	res := repo.db.WithContext(ctx).Find(answer)
-
-	if err := res.Error; err != nil {
-		repo.logger.Error("error get answer",
-			zap.String("answer_id", id.String()),
-			zap.Error(err))
-
-		return nil, err
-	}
-
-	return answer, nil
 }
